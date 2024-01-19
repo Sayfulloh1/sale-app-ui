c1324c065ae261d046df2eef4bc1d7799ab1d34b
@Ibroximjon2003 <maxammadjonovibroximjon24@gmail.com>
2024-01-19 21:17:40 +0500

    gridview example ui

---
+++ android/app/src/main/kotlin/com/example/ibroxim/sale_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ibroxim.sale_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> imageItems = [
+    'assets/images/image (1).jpg',
+    'assets/images/image (2).jpg',
+    'assets/images/image (3).jpg',
+    'assets/images/image (4).jpg',
+    'assets/images/image (5).jpg',
+    'assets/images/image (6).jpg',
+    'assets/images/image (7).jpg',
+    'assets/images/image (8).jpg',
+    'assets/images/image (9).jpg',
+    'assets/images/image (10).jpg',
+    'assets/images/image (11).jpg',
+    'assets/images/image (12).jpg',
+    'assets/images/image (13).jpg',
+    'assets/images/image (14).jpg',
+    'assets/images/image (15).jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[700],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Home'),
+        centerTitle: true,
+        leading: const Icon(
+          Icons.menu,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text('0'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/image (1).jpg'),
+                      fit: BoxFit.cover,
+                    )),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomRight,
+                      colors: [
+                        Colors.black.withOpacity(.6),
+                        Colors.black.withOpacity(.5),
+                        Colors.black.withOpacity(.1),
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Lifestyle sale',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40),
+                      ),
+                      const SizedBox(height: 30),
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Shop now',
+                            style: TextStyle(
+                              color: Colors.grey[800],
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Expanded(
+                child: GridView.count(
+
+                  padding: const EdgeInsets.all(10),
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  children: imageItems
+                      .map(
+                        (image) => Card(
+                          color: Colors.transparent,
+                          elevation: 0,
+
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                image: AssetImage(image),
+                                fit: BoxFit.cover,
+                              )
+                            ),
+                            child: Transform.translate(
+                              offset: const Offset(50,-70),
+                              child: Container(
+                                margin: const EdgeInsets.all(60),
+
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.white,
+                                ),
+                                child: const Icon(Icons.bookmark_border),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
